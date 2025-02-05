aa04d76fe35782e66710dc56105d2e25495c82f7
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2025-02-05 21:40:25 +0200

    Setup project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './portfolio/components/main-layout/main-layout.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { BlogComponent } from './blog/components/blog/blog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainLayoutComponent, children: [] },
+  { path: 'blog', component: BlogComponent, children: [] },
+  { path: '**', component: NotFoundComponent },
+];
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["lofi"], // or "night" for a slightly different black theme
+  },
+};
