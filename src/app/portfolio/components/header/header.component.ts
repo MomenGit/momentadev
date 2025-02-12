df1ba238b4c3325f1b533438b8c97fd49680506a
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2025-02-13 01:13:54 +0200

    Add lang controller component

--- src/app/portfolio/components/header/header.component.ts
+++ src/app/portfolio/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { LangControllerComponent } from '../../../shared/components/lang-controller/lang-controller.component';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, LangControllerComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
