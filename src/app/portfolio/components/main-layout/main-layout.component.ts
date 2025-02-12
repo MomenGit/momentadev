a84a470d540f515e286544dbbdb0b8fe79ea6667
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2025-02-12 21:14:59 +0200

    feat: create portfolio main layout

--- src/app/portfolio/components/main-layout/main-layout.component.ts
+++ src/app/portfolio/components/main-layout/main-layout.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './main-layout.component.html',
-  styleUrl: './main-layout.component.css'
+  styleUrl: './main-layout.component.css',
 })
-export class MainLayoutComponent {
-
-}
+export class MainLayoutComponent {}
