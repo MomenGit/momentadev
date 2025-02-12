d575299eaab07b3ad7ede8fd82b39ace472de488
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2025-02-12 23:23:45 +0200

    Initialize blog page

--- src/app/blog/components/blog/blog.component.ts
+++ src/app/blog/components/blog/blog.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { BlogCardComponent } from '../blog-card/blog-card.component';
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [HeaderComponent, BlogCardComponent],
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.css'
+  styleUrl: './blog.component.css',
 })
 export class BlogComponent {
-
+  listaia = new Array(5);
 }
