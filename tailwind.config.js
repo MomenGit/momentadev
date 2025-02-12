b87ed759e09e6eccef9b0d50aab96b3b3d1f078e
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2025-02-12 20:53:21 +0200

    Setup styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
